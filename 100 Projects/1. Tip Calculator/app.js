63cb0ce57eeeeb009156ad46e232a183df57fd8a
Edward Dzongololo <98263200+EdwardDzongololo@users.noreply.github.com>
2023-10-20 12:37:44 +0300

    .
    .

---
+++ 100 Projects/1. Tip Calculator/app.js
@@ -0,0 +1,24 @@
+const total=document.getElementById("total")
+const calculateBtn=document.getElementById("calculateBtn")
+const againBtn=document.getElementById("enterAgainBtn")
+let bill=document.getElementById("bill")
+const percentage=document.getElementById("percentage")
+
+
+calculateBtn.addEventListener("click",function(){
+const billInput=bill.value
+const percentageInput=percentage.value
+if(billInput<=0){
+    document.querySelector(".error").style.display = "block"
+   againBtn.style.display="block"
+
+
+   againBtn.addEventListener("click", function(){
+    againBtn.style.display="none"
+    document.querySelector(".error").style.display = "none"
+    bill.value=""
+   })
+}
+total.innerText='$ '+billInput*percentageInput/100
+   
+})
