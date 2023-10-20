1f6825c9ccee32cb00a2c19014b002774c23039a
Edward Dzongololo <98263200+EdwardDzongololo@users.noreply.github.com>
2023-10-20 12:16:18 +0300

    .
    .

--- 100 Projects/Tip Calculator(project 1)/app.js
+++ 100 Projects/Tip Calculator(project 1)/app.js
@@ -1,10 +1,24 @@
 const total=document.getElementById("total")
 const calculateBtn=document.getElementById("calculateBtn")
+const againBtn=document.getElementById("enterAgainBtn")
+let bill=document.getElementById("bill")
+const percentage=document.getElementById("percentage")
 
-let bill=document.getElementById("bill").value
-const percentage=document.getElementById("percentage").value
 
 calculateBtn.addEventListener("click",function(){
-  console.log(bill+2)
-  
+const billInput=bill.value
+const percentageInput=percentage.value
+if(billInput||percentage<=0){
+    document.querySelector(".error").style.display = "block"
+   againBtn.style.display="block"
+
+
+   againBtn.addEventListener("click", function(){
+    againBtn.style.display="none"
+    document.querySelector(".error").style.display = "none"
+    bill.value=""
+   })
+}
+total.innerText='$ '+billInput*percentageInput/100
+   
 })
